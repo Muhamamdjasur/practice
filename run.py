6c5bd6418052f68fcdac73369d411b0fec54cd47
Muhammadjasur <jasurmuhammadjonov1122@gmail.com>
2026-09-13 04:48:43 +0900

    feat: everything is object

---
+++ run.py
@@ -0,0 +1,15 @@
+# Dunder builtins, init
+message = "PYTHON: Everything is object!"
+print(message)
+
+result = type(message)
+print("result:", result)
+
+"""
+In Python, there are builtin tools:
+(1) TYPES > int float str list dict
+(2) FUNCTIONS > print() len() input() type() str() int()
+(3) CONSTANTS > True False None
+"""
+
+print(dir(builtins))
